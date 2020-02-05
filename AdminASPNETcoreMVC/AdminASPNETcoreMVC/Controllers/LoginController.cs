3da045fc8aead4e709b770391a293d261a627761
roua abid <abid.rourou@gmail.com>
2020-02-05 21:53:56 +0100

    partie admin

---
+++ AdminASPNETcoreMVC/AdminASPNETcoreMVC/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminASPNETcoreMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Login(LoginViewModel model String returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSingInAsync(model.Email, model.Password, model.rememberMe, false);
+                if(result.Succeeded)
+                {
+                    if(!String.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("index", "home");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
